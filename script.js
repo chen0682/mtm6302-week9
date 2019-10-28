bf2541ff76b6a969d614c6707e5641175818fbf8
chen0682 <chen0682@algonquinlive.com>
2019-10-28 12:03:20 -0400

    Practices

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 const fruits = ['apples', 'bananas', 'cherries']
 const colors = ['red', 'green', 'blue', 'purple', 'orange']
+
+// Find the h1 tag
+const $header = document.getElementById('header')
+console.log([$header])
+
+const $buttons = document.querySelectorAll('.button')
+console.log($buttons)
+
+for (const $button of $buttons) {
+  $button.style.backgroundColor = 'blue'
+}
+
+const $list = document.getElementById('list')
+console.log($list.children[1])
+const $bread = document.querySelector('li:nth-of-type(2)')
+console.log($bread)
+
+const $item = document.querySelector('li')
+console.log($item.nextElementSibling)
+
+$header.textContent = 'Hello World!'
+$header.classList.add('highlight')
+
+$header.style.color = 'red'
+$header.style.backgroundColor = 'yellow'
